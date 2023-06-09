5ccbc3a4cc87fecebbf30bf15c06815770a682eb
Florian Froese <ffroese@adobe.com>
2023-06-10 00:09:24 +0200

    Add embed video

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,6 +22,63 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
 //   // no-op
 // }
 
+const loadScript = (url, callback, type) => {
+  const head = document.querySelector('head');
+  const script = document.createElement('script');
+  script.src = url;
+  if (type) {
+    script.setAttribute('type', type);
+  }
+  script.onload = callback;
+  head.append(script);
+  return script;
+};
+
+const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+    <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
+      scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
+    </iframe>
+  </div>`;
+
+const embedYoutube = (url, autoplay) => {
+  const usp = new URLSearchParams(url.search);
+  const suffix = autoplay ? '&muted=1&autoplay=1' : '';
+  let vid = encodeURIComponent(usp.get('v'));
+  const embed = url.pathname;
+  if (url.origin.includes('youtu.be')) {
+    [, vid] = url.pathname.split('/');
+  }
+  const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+      <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+      allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
+    </div>`;
+  return embedHTML;
+};
+
+const loadEmbed = (block, link, autoplay) => {
+  if (block.classList.contains('embed-is-loaded')) {
+    return;
+  }
+
+  const EMBEDS_CONFIG = [
+    {
+      match: ['youtube', 'youtu.be'],
+      embed: embedYoutube,
+    },
+  ];
+
+  const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
+  const url = new URL(link);
+  if (config) {
+    block.innerHTML = config.embed(url, autoplay);
+    block.classList = `block embed embed-${config.match[0]}`;
+  } else {
+    block.innerHTML = getDefaultEmbed(url);
+    block.classList = 'block embed';
+  }
+  block.classList.add('embed-is-loaded');
+};
+
 /**
  * Wraps images followed by links within a matching <a> tag.
  * @param {Element} container The container element
@@ -31,8 +88,24 @@ export function wrapImgsInLinks(container) {
   pictures.forEach((pic) => {
     const link = pic.nextElementSibling;
     if (link && link.tagName === 'A' && link.href) {
-      link.innerHTML = pic.outerHTML;
-      pic.replaceWith(link);
+      if (link.href.includes("youtube.com")) {
+        link.innerHTML = pic.outerHTML;
+
+        const parent = pic.parentNode;
+        parent.textContent = '';
+        const wrapper = document.createElement('div');
+        wrapper.className = 'embed-placeholder';
+        // wrapper.innerHTML = '<div class="embed-placeholder-play"><button title="Play"></button></div>';
+        wrapper.prepend(pic);
+        wrapper.addEventListener('click', () => {
+          loadEmbed(parent, link.href, true);
+        });
+        parent.append(wrapper);
+
+      } else {
+        link.innerHTML = pic.outerHTML;
+        pic.replaceWith(link);
+      }
     }
   });
 }
