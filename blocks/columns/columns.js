641985f88512f984623d670c92bd9188b9a227b0
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-09 12:59:19 +0200

    columns styling lists images

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -13,11 +13,24 @@ export default function decorate(block) {
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
-          if (block.classList.contains('spray')) {
-            picWrapper.append(createOptimizedPicture('/images/mister.png', 'Flumist Spray', false, [{ width: '750' }]));
-          }
+          // if (block.classList.contains('spray')) {
+          //   picWrapper.append(createOptimizedPicture('/images/mister.png', 'Flumist Spray', false, [{ width: '750' }]));
+          // }
         }
       }
+      [...col.querySelectorAll('picture')].forEach((picture) => {
+        picture.parentElement.classList.add('picture');
+      });
+      [...col.querySelectorAll('p.picture + h4 + p')].forEach((imgH4P) => {
+        const imgH4PContainer = document.createElement('div');
+        imgH4PContainer.classList.add('imgH4P');
+        imgH4P.after(imgH4PContainer);
+        imgH4PContainer.append(imgH4P.previousSibling.previousSibling.previousSibling.previousSibling, imgH4P.previousSibling.previousSibling, imgH4P);
+      });
     });
   });
+  if (block.classList.contains('spray')) {
+    const sprayPicture = block.querySelector(':scope > div > div:last-of-type picture');
+    if (sprayPicture) sprayPicture.parentElement.append(createOptimizedPicture('/images/mister.png', 'Flumist Spray', false, [{ width: '750' }]));
+  }
 }
